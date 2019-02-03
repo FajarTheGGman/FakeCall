5503282184cbb4ce4ede38e1efd42713f5d774d5
FajarFirdaus732 <35714197+FajarTheGGman@users.noreply.github.com>
2019-02-03 09:12:12 +0700

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -19,3 +19,16 @@ echo "[==============================]"
 
 read -p "Masukan Nomor yang mau di spam : " no
 dick $no
+
+#Dear Tukang Recode :
+#[=============Baca Hadist ini=============]
+#Lelaki yang mencuri dan wanita yang mencuri,potonglah tangan
+#keduanya (sebagai) pembalasan bagi apa yang mereka kerjakan dan
+#sebagai siksaan dari Allah.
+#Dan Allah Maha Perkasa lagi Maha Bijaksana.
+#Maka barangsiapa bertaubat (di antara pencuri-pencuri itu)
+#sesudah melakukan kejahatan itu dan memperbaiki diri,
+#maka sesungguhnya Allah menerima taubatnya.
+#Sesungguhnya Allah Maha Pengampun lagi Maha Penyayang.”
+
+#(QS. Al-Maidah: 38-39)
