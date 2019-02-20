4b4fee4e4dd6b343dde9bc132544dc81d28f67e8
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-02-20 19:19:47 +0700

    Add files via upload

---
+++ spam.php
@@ -0,0 +1,27 @@
+<?php
+echo "
+[====================]
+Author : Fajar Firdaus
+Fb : Fajar Firdaus
+YT : iTech7732
+IG : fajar_firdaus_7
+[====================]
+";
+function api($nomor,$jumlah){
+$url = "https://0x.nakocoders.org/rest-api/lain-nya/api.php?nomor=$nomor";
+$loop = 0;
+while($loop < $jumlah){
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$hasil = curl_exec($ch);
+echo $hasil;
+$loop++;
+}
+}
+echo "Masukan Nomor : ";
+$nomor = trim(fgets(STDIN));
+echo "Masukan Jumlah : ";
+$jumlah = trim(fgets(STDIN));
+api($nomor,$jumlah);
+?>
